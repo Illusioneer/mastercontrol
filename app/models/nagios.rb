261395b80f143f5e71a8317853763adcdf4448ff
bkowens <mrtransformations@gmail.com>
2013-01-08 16:30:01 -0500

    Added graphing info

--- app/models/nagios.rb
+++ app/models/nagios.rb
@@ -5,4 +5,10 @@ class Nagios
   field  :timestamp,  :type => Integer
   field  :nagiosdata, :type =>  Hash
 
+  def self.service_history(servicename)
+      dib = Array.new
+      Nagios.all.each{|entry| [entry.servicestatus.each{|item| dib << [item['host_name'], item['check_latency']] unless item['host_name'] != "pub-dashboard-dev" }]}
+      return dib
+  end
+  
 end
