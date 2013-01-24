73459c7fad3c23b12e7e03f3808ab3ad0db2c7ee
bkowens <mrtransformations@gmail.com>
2013-01-24 15:12:02 -0500

    Changes to instance controller.

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -44,7 +44,7 @@ class InstancesController < ApplicationController
 
     respond_to do |format|
       if @instance.save
-        format.html { redirect_to "/instanecs/#{@instance.name}/show", notice: "Instance was successfully created." }
+        format.html { redirect_to "/instances", notice: "Instance was successfully created." }
         format.json { render json: @instance, status: :created, location: @instance }
       else
         format.html { render action: "new" }
