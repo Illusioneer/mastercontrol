3feced8ed90cfb59c81fed94747a3d0c961f9953
bkowens <mrtransformations@gmail.com>
2013-02-06 15:13:36 -0500

    initial report form commit

--- app/models/nagios.rb
+++ app/models/nagios.rb
@@ -32,7 +32,7 @@ class Nagios
   def self.uptime(datestart, dateend, servicename)
     uptick = 0
     downtick = 0
-    Nagios.each do |services|
+    Nagios.all.each do |services|
         services.servicestatus.each do |service|
            service["host_name"]==servicename and service["service_description"]== "HTTP" and service["current_state"]== 0 ? uptick += 1: downtick += 1
         end
