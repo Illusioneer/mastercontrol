14abb8a275c1bc560c1b3752b0e0c531fc5bd1c7
bkowens <mrtransformations@gmail.com>
2013-02-14 17:30:52 -0500

    Broke up statuses to 3 separate models.

--- app/models/nagios.rb
+++ app/models/nagios.rb
@@ -44,5 +44,4 @@ class Nagios
     return downtick/uptick
 
   end
-  
 end
--- app/models/servicestatus.rb
+++ app/models/servicestatus.rb
@@ -1,6 +1,17 @@
 class Servicestatus
   include Mongoid::Document
-  #field
-  #field
+
+  field :host_name, :type => String
+  field :service_description, :type => Integer
+  field :check_command, :type => String
+  field :check_execution_time, :type => String
+  field :check_latency, :type => String
+  field :check_type, :type => Integer
+  field :current_state, :type => Integer
+  field :current_event_id, :type => Integer
+  field :state_type, :type => Integer
+  field :plugin_output, :type => String
+
+  
   embedded_in :nagios, :inverse_of => :servicestatuses
 end
